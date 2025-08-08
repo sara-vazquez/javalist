307d65b52b4c8d3065857f9726ae4ce5ff1a5bef
Sara Vázquez <saravazlo@gmail.com>
2025-08-08 12:46:21 +0200

    test-test passed

--- src/test/java/dev/sara/JavaListTest.java
+++ src/test/java/dev/sara/JavaListTest.java
@@ -2,18 +2,21 @@ package dev.sara;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-
+import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
 
 public class JavaListTest {
     
-    private JavaList javaList;
+    public JavaList javaList;
 
     @Test
     void testCreate_Days() {
 
+        javaList = new JavaList();
+
         assertThat(javaList.days, hasSize(7));
         assertThat(javaList.days.get(0), is("Lunes"));
         assertThat(javaList.days.get(6), is("Domingo"));
+
     }   
 }
