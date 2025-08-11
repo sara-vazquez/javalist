a0ade9f51494edbd1a8ad1b44c5534e4dc73a913
Sara Vázquez <saravazlo@gmail.com>
2025-08-11 17:25:25 +0200

    test-refactor

--- src/test/java/dev/sara/JavaListTest.java
+++ src/test/java/dev/sara/JavaListTest.java
@@ -8,16 +8,20 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class JavaListTest {
     
-    public JavaList javaList;
+    private JavaList javaList;
+
+    @BeforeEach
+    void setUp() {
+        javaList = new JavaList();
+    }
 
     @Test
     void testCreate_Days() {
-
-        javaList = new JavaList();
 
         assertThat(javaList.getDays(), hasSize(7));
         assertThat(javaList.getDays(), contains("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"));
@@ -25,7 +29,6 @@ public class JavaListTest {
 
     @Test
     void testGetDaysCount() {
-        JavaList javaList = new JavaList();
 
         assertThat(javaList.getDaysCount(), is(7));
     }
@@ -33,29 +36,24 @@ public class JavaListTest {
     @Test
     void testDelete_Days() {
 
-        JavaList javaList = new JavaList();
-
         assertThat(javaList.deleteDay("Martes"), is(true));
         assertThat(javaList.getDays(), not(hasItem("Martes")));
     }
 
     @Test
     void testGetDay() {
-    JavaList javaList = new JavaList();
 
     assertThat(javaList.getDayByName("Miércoles"), is(Optional.of("Miércoles")));
 }
 
     @Test
     void testConteinsDay() {
-        JavaList javaList = new JavaList();
 
         assertThat(javaList.containsDay("Jueves"), is(true));
     }
 
     @Test
     void testSortAlphabetically() {
-        JavaList javaList = new JavaList();
 
         javaList.sortDaysAlphabetically();
         assertThat(javaList.getDays(), contains("Domingo", "Jueves", "Lunes", "Martes", "Miércoles", "Sábado", "Viernes"));
@@ -63,7 +61,6 @@ public class JavaListTest {
 
     @Test
     void testClearDays() {
-        JavaList javaList = new JavaList();
 
         javaList.clearDays();
         assertThat(javaList.getDays(), hasSize(0));
