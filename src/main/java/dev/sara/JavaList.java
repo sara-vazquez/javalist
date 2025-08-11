12e1bfb6fb4ff335ea78f1a68dd5dc649a39366b
Sara Vázquez <saravazlo@gmail.com>
2025-08-11 17:17:04 +0200

    fix-updated

--- src/main/java/dev/sara/JavaList.java
+++ src/main/java/dev/sara/JavaList.java
@@ -58,20 +58,8 @@ public class JavaList {
 /* 
 * 1. La clase debe tener los siguientes métodos:
 
-* Un método para crear la lista de los días de la semana ✔︎
-* Un método que retorne los días de la semana ✔︎
-* Un método que retorne el largo de la lista ✔︎
-* Un método para eliminar un día de la semana ✔︎
-* Un método que retorne el día de la semana solicitado ✔︎
-* Un método que retorne si el día solicitado existe en la lista ✔︎
-* Un método para ordenar la lista de días por orden alfabético
 * Un método para vaciar la lista
 
 
 Requisitos :
-* Se debe realizar un test unitario de cada método
-* Se debe utilizar la colección "List" de java.util
-
-Entrega :
-Repositorio de Github
- */
+* Se debe realizar un test unitario de cada método */
